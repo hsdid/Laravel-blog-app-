d5c4bd110860805a179fab01638d94487dab9abe
Mateusz Murawski <murawskimat00@gmail.com>
2020-08-24 23:02:11 +0200

    Access Control

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,18 +1,30 @@
 @extends('layouts.app')
 @section('content')
-    <a href="/posts" class="btn btn-primary mb-3">Go Back</a>
+<a href="/posts" class="btn btn-primary m-3">Go Back</a>
+<div class="card">
+    
+    
+    
+    <div class="card-header">
     <h1>{{$post->title}}</h1>
-    <div>
-        {{$post->body}}
     </div>
+    <div class="card-body">
+        {{$post->body}}
+    
     <hr>
     <small>Written on {{$post->created_at}} by <b>{{$post->user->name}}</b></small>
     <hr>
-    {{-- <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
-    {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method'=>'POST', 'class' => ' float-right'])!!}
-        {{-- {{Form::hidden('_method','DELETE')}} --}}
-        {{-- {{ Form::open(array('url' => '/', 'method' => 'DELETE', 'class'=>'col-md-12')) }}
-        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
-    
-    {!! Form::close() !!} --}}
+    @if(!Auth::guest())
+        @if(Auth::user()->id == $post->user_id)
+            <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
+                {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method'=>'POST', 'class' => ' float-right'])!!}
+                {{Form::hidden('_method','DELETE')}}
+                {{ Form::open(array('url' => '/', 'method' => 'DELETE', 'class'=>'col-md-12')) }}
+                {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+            
+            {!! Form::close() !!}
+        @endif
+    @endif
+    </div>  
+</div>
 @endsection
